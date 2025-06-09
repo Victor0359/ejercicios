a43bd802351ae16c774ec73941b49d9b80edf4b6
Victor9530 <garrotevicgmail.com>
2025-06-09 19:25:45 -0300

    hoy

--- src/datadb.js
+++ src/datadb.js
@@ -2,7 +2,7 @@ const  {pool}  = require('pg');
 
 
 const pool = new pg.createPool({
-    host: 'dpg-d10a6r2li9vc73dkq9t0-a.oregon-postgres.render.com/alquileres_1kec', // IP de la base de datos
+    host: 'dpg-d10a6r2li9vc73dkq9t0-a.oregon-postgres.render.com', // IP de la base de datos
     user: 'alquileres_1kec_user', // Usuario de la base de datos           
     password: 'TAuI9t1Gw4sXuiTGj3UHQDM6lx7RDkeP', // Contraseña de la base de datos
     database: 'alquilers_1Kec',
