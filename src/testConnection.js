ae9cc6e1a0c23bf111d5694863685f8f757b32a4
Victor9530 <garrotevicgmail.com>
2025-06-09 19:49:16 -0300

    restituyo test connection

---
+++ src/testConnection.js
@@ -0,0 +1,23 @@
+const database = require('./datadb');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+(async () => {
+  try {
+    const conn = await database.conectar();
+    const resultado = await conn.query("SELECT 1");
+    console.log("Conexión exitosa y prueba de consulta:", resultado);
+    conn.end();
+  } catch (err) {
+    console.error("Error al probar la conexión:", err);
+  }
+})();
