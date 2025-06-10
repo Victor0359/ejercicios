b59357a651afeb65d6be336b6496ecc2bdb6ce93
Victor9530 <garrotevicgmail.com>
2025-06-10 12:37:20 -0300

    cmbios en index.js

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ const contratos = require("./contratos");
 const recibo_contrato = require("./recibo_contrato");
 
 const app = express();
-const port = 3000
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
--- src/testConnection.js
+++ src/testConnection.js
@@ -1,7 +1,7 @@
 const database = require('./datadb');
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
