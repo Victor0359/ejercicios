6e73f8daef37ecedb82c17b47d9808d5f6bd75b4
Victor9530 <garrotevicgmail.com>
2025-06-10 14:56:45 -0300

    nuevo arreglo datadb.js

--- src/datadb.js
+++ src/datadb.js
@@ -1,16 +1,26 @@
 const { Pool } = require('pg');
 
 
-const pool = new Pool ({
-    host: 'dpg-d10a6r2li9vc73dkq9t0-a.oregon-postgres.render.com', // IP de la base de datos
-    user: 'alquileres_1kec_user', // Usuario de la base de datos           
-    password: 'TAuI9t1Gw4sXuiTGj3UHQDM6lx7RDkeP', // Contraseña de la base de datos
+const pool = new Pool({
+    host: 'dpg-d10a6r2li9vc73dkq9t0-a.oregon-postgres.render.com',
+    user: 'alquileres_1kec_user',
+    password: 'TAuI9t1Gw4sXuiTGj3UHQDM6lx7RDkeP',
     database: 'alquilers_1Kec',
-    charset: 'utf8mb4', // Nombre de la base de datos
-    port:5432,
-    ssl: { rejectUnauthorized: false } 
-
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
 });
+
+async function conectar() {
+   try {
+    const conn = await pool.connect();
+    console.log("Conexión exitosa!");
+    return conn;
+  } catch (err) {
+    console.error("Error de conexión:", err);
+    throw err;
+  }
+}
+
 
 async function conectar() {
    try {
