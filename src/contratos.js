30491dadde8a20686530231ff883327765258eee
Victor9530 <garrotevicgmail.com>
2025-10-07 17:01:29 -0300

    nuevo 07/10/2025

--- src/contratos.js
+++ src/contratos.js
@@ -160,11 +160,13 @@ async function obtenerContratosPorIdPropiedad(id_propiedad) {
   try {
     const resultado = await pool.query(
       `SELECT *,
-        ((DATE_PART('year', CURRENT_DATE) - DATE_PART('year', fecha_inicio)) * 12 +
-         (DATE_PART('month', CURRENT_DATE) - DATE_PART('month', fecha_inicio))) AS cuota,honorarios
-       FROM contratos
-       WHERE id_propiedades = $1
-       ORDER BY fecha_inicio DESC`,
+       (DATE_PART('year', AGE(CURRENT_DATE, fecha_inicio)) * 12 +
+        DATE_PART('month', AGE(CURRENT_DATE, fecha_inicio)) + 1) AS cuota,
+       honorarios
+FROM contratos
+WHERE id_propiedades = $1
+ORDER BY fecha_inicio DESC;
+`,
       [id_propiedad]
     );
     return resultado.rows;
